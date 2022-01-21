1b16e972e483caf178fd30e4bc648282aca2ff69
Seyyed Soroosh Hosseinalipour <soorosh_abi@hotmail.com>
2022-01-21 11:26:04 +0000

    Installing LocalPV engine for OpenEBS. (fixes #13)

--- Deployment/Kubernetes/install-openebs.sh
+++ Deployment/Kubernetes/install-openebs.sh
@@ -23,7 +23,7 @@ fi
 
 # Check kubectl cli exist or not?
 # https://stackoverflow.com/a/677212/1539100
-if command -v kubectl &> /dev/null ; then
+if ! command -v kubectl &> /dev/null ; then
     echo "kubectl does not found, please install kubectl, and ensure K8s running on this node"
     exit 1
 fi
@@ -33,6 +33,11 @@ fi
 if command -v helm &> /dev/null && [[ is_internet_exist ]] ; then
     echo "Helm found, so we using helm..."
 
+    if [[ $(helm ls -n openebs | wc -l) -gt 1 ]] ; then
+        echo "OpenEBS is installed, so we do not need reinstall"
+        exit 0
+    fi
+
     # Check helm needed repo exist or not?
     if ! helm repo list | grep openebs &> /dev/null ; then
         # Before I can install the chart I will need to add the harbor repo to Helm
@@ -40,8 +45,18 @@ if command -v helm &> /dev/null && [[ is_internet_exist ]] ; then
     fi
 
     # Follow https://medium.com/volterra-io/kubernetes-storage-performance-comparison-v2-2020-updated-1c0b69f0dcf4 instructions
-    #‌ to prepare environment for installing OpenEBS
+    #‌ to prepare environment for installing OpenEBS run configure-hugepages.sh in each node
 
+    # TODO: Optional for installing mayastor as OpenEBS engine
+    for node_name in $(kubectl get nodes --no-headers | tr -s ' ' | grep none | cut -d ' ' -f 1) ; do
+        kubectl label node $node_name openebs.io/engine=mayastor
+    done
+
+    # For checking that node has correct label or not? you can use kubectl label --list nodes node_name
+
+    # For checking helm chart documentation please see https://github.com/openebs/charts/tree/master/charts/openebs
+    # default installation of openebs, install LocalPV
+    helm install openebs --namespace openebs openebs/openebs --create-namespace
 else
     echo "Helm must be install from 'install-helm.sh'"
 fi
