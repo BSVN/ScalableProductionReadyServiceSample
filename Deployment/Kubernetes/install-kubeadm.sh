0f26fde168d337342ebbbf43f8ab6453b4096ecc
Ebrahim <meghasemim1999@gmail.com>
2023-05-30 09:47:19 +0330

    Changes made to make scripts better

--- Deployment/Kubernetes/install-kubeadm.sh
+++ Deployment/Kubernetes/install-kubeadm.sh
@@ -31,11 +31,10 @@ then
 	sudo apt-get install -y apt-transport-https ca-certificates curl
 
 	# Download the Google Cloud public signing key:
-	sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-
+	sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 	# Add the Kubernetes apt repository:
 	# TODO: xenial must based on edition
-	echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+	echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 	# Update apt package index, install kubelet, kubeadm and kubectl, and pin their version:
 	sudo apt-get update
