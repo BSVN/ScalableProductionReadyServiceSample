ee8a809f45672f8710380d3b3caf0acf8a175479
Ebrahim <meghasemim1999@gmail.com>
2023-06-15 14:40:28 +0330

    Update install-kubeadm.sh and undo unneccessary changes

--- Deployment/Kubernetes/install-kubeadm.sh
+++ Deployment/Kubernetes/install-kubeadm.sh
@@ -31,14 +31,11 @@ then
 	sudo apt-get install -y apt-transport-https ca-certificates curl
 
 	# Download the Google Cloud public signing key:
-	sudo curl -fsSLo https://packages.cloud.google.com/apt/doc/apt-key.gpg 
-	
-	# Add key
-	sudo apt-key add apt-key.gpg
+	sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 
 	# Add the Kubernetes apt repository:
 	# TODO: xenial must based on edition
-	echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+	echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 	# Update apt package index, install kubelet, kubeadm and kubectl, and pin their version:
 	sudo apt-get update
